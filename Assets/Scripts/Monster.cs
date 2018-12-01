426c483920682842b0a49e0c833c3f3ac0256107
Wacken <wacken>
2018-12-01 03:59:39 +0100

    My first ai

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+/* Makes enemies follow and attack the player */
+
+public class Monster : MonoBehaviour
+{
+
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    enum MonsterState
+    {
+
+    }
+
+    void Start()
+    {
+        target = Player.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // Get the distance to the player
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        // If inside the radius
+        if (distance <= lookRadius)
+        {
+            // Move towards the player+
+            agent.SetDestination(target.position);
+            if (distance <= agent.stoppingDistance)
+            {
+                // Attack
+
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/* This class just makes it faster to get certain components on the player. */
+
+public class Player : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static Player instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+}
