9ad3aa6f77d0a7051065f9abdad6b66ef3c1d2ad
Kjell <the_underpro@gmx.net>
2018-12-01 05:51:39 +0100

    Start of xray shader and playerToShader script

---
+++ Assets/Scripts/PlayerToShader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerToShader : MonoBehaviour {
+    public GameObject PlayerObject;
+	// Use this for initialization
+	void Start () {
+        if (PlayerObject == null)
+        {
+            PlayerObject = this.gameObject;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Color pos = new Color(PlayerObject.transform.position.x, PlayerObject.transform.position.y, PlayerObject.transform.position.z);
+        Shader.SetGlobalColor("_PlayerPos", pos);
+    }
+}
