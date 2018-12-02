519a80d6e2706494b968f8d132663ca4a29e8573
Wacken <wacken>
2018-12-02 11:11:23 +0100

    Sound fix server and monster

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -66,7 +66,7 @@ public class Monster : MonoBehaviour
                 if (distance <= _smellRadiusVisor)
                 {
                     _agent.SetDestination(_patroulPath[0].position);
-                    _agent.speed = 100;
+                    _agent.speed = 50;
                 }
                 else
                 {
