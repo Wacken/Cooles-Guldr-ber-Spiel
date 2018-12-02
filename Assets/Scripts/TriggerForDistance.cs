e5942ad7627b485f692dc8eeecbd4e4f8f7af8ba
Wacken <wacken>
2018-12-02 08:51:52 +0100

    Monster and Trigger for distance

---
+++ Assets/Scripts/TriggerForDistance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerForDistance : MonoBehaviour {
+
+    float _radius;
+    [SerializeField]
+    AudioSource _source;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float distance = Vector3.Distance(Player.instance._player.transform.position, transform.position);
+        if(distance < _radius)
+        {
+            _source.Play();
+        }
+    }
+}
